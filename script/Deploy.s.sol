bc32e8015f2fd861f7754ad0e67a3e46fe2d41d2
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-05-26 02:23:21 -0500

    integrate lagrangecommittee into deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,10 +6,12 @@ import "forge-std/Test.sol";
 import {IStrategy} from "eigenlayer-contracts/interfaces/IStrategy.sol";
 import {StrategyManager} from "eigenlayer-contracts/core/StrategyManager.sol";
 import {IDelegationTerms} from "eigenlayer-contracts/interfaces/IDelegationTerms.sol";
+import {IServiceManager} from "eigenlayer-contracts/interfaces/IServiceManager.sol";
 import {DelegationManager} from "eigenlayer-contracts/core/DelegationManager.sol";
 
 import {Slasher} from "eigenlayer-contracts/core/Slasher.sol";
-import {LagrangeService} from "src/protocol/LagrangeService/LagrangeService.sol";
+import {LagrangeService} from "src/protocol/LagrangeService.sol";
+import {LagrangeCommittee} from "src/protocol/LagrangeCommittee.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -20,18 +22,42 @@ contract Deploy is Script, Test {
                 "lib/eigenlayer-contracts/script/output/M1_deployment_data.json"
             )
         );
-    address WETHStractegyAddress;
-
-    function run() public {
-        vm.startBroadcast(msg.sender);
-
-        string memory deployData = vm.readFile(deployDataPath);
+    string public poseidonDataPath =
+        string(
+            bytes(
+                "util/output/poseidonAddresses.json"
+            )
+        );
         
+    address WETHStrategyAddress;
+    address IServiceManagerAddress;
+    
+    function deployLagrangeCommittee() public returns (LagrangeCommittee) {
+        string memory poseidonData = vm.readFile(poseidonDataPath);
+        // Load Poseidon contract addresses
+        address poseidon2 = stdJson.readAddress(poseidonData, ".2");
+        address poseidon3 = stdJson.readAddress(poseidonData, ".3");
+        address poseidon4 = stdJson.readAddress(poseidonData, ".4");
+        // Initialize and deploy LagrangeCommittee
+        LagrangeCommittee lagrangeCommittee = new LagrangeCommittee();
+        lagrangeCommittee.initialize(poseidon2,poseidon3,poseidon4);
+        console.log("LagrangeService deployed at: ", address(lagrangeCommittee));
+        
+        return lagrangeCommittee;
+    }
+
+    function deployLagrangeService(LagrangeCommittee lagrangeCommittee) public {
+        string memory deployData = vm.readFile(deployDataPath);
+        // Load Dependencies
+        //IServiceManager ServiceManager = 
+        //
         // Deploy LagrangeService
+        
         Slasher slasher = Slasher(
             stdJson.readAddress(deployData, ".addresses.slasher")
         );
         LagrangeService service = new LagrangeService(slasher);
+        service.initialize(lagrangeCommittee/*, serviceManager, wethStrategy*/);
         console.log("LagrangeService deployed at: ", address(service));
 
         // call optIntoSlashing on slasher
@@ -39,7 +65,14 @@ contract Deploy is Script, Test {
         slasher.optIntoSlashing(address(service));
 
         // register the service
-        service.register(type(uint32).max);
+        service.register(type(uint32).max);        
+    }
+
+    function run() public {
+        vm.startBroadcast(msg.sender);
+
+	LagrangeCommittee lagrangeCommittee = deployLagrangeCommittee();
+	deployLagrangeService(lagrangeCommittee);
 
         vm.stopBroadcast();
     }
