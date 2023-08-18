fed4f75cd3f1743741d1a3ef4b507da9bc75f5c1
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-08-18 11:53:05 -0500

    add optimism verifier interface

---
+++ src/interfaces/IOptimismVerifier.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {IRecursiveHeaderVerifier} from "../interfaces/IRecursiveHeaderVerifier.sol";
+
+interface IOptimismVerifier {
+    function getOutputHash(
+        bytes32[4] memory outputProof
+    ) external view returns (bytes32);
+
+    function verifyOptBlock(
+        bytes memory rlpData,
+        uint256 comparisonNumber,
+        bytes32 comparisonBlockHash,
+	bytes memory headerProof,
+        bytes calldata extraData,
+        IRecursiveHeaderVerifier RHVerify
+    ) external view returns (bool);
+    
+    function verifyOutputProof(
+        uint256 comparisonNumber,
+        bytes32 comparisonBlockHash,
+        bytes calldata extraData
+    ) external view returns (bool,bytes32);
+}
