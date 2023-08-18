2f9bddd17723db14118a234d5dd61ecfda727fe5
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-08-18 02:27:28 -0500

    update pre-test deployment process

--- test/hardhat/LagrangeCommittee.test.js
+++ test/hardhat/LagrangeCommittee.test.js
@@ -60,9 +60,34 @@ describe("LagrangeCommittee", function () {
     });
 
     beforeEach(async function () {
-        const LagrangeCommitteeFactory = await ethers.getContractFactory("LagrangeCommittee");
-        const committee = await LagrangeCommitteeFactory.deploy(admin.address);
-        await committee.deployed();
+        const overrides = {
+            gasLimit: 5000000,
+        };
+
+	//const Common = await ethers.getContractFactory("Common");
+	//const common = await Common.deploy();
+	//await common.deployed();
+          
+        console.log("Deploying Slasher mock...");
+
+        const SlasherFactory = await ethers.getContractFactory("Slasher");
+        const slasher = await SlasherFactory.deploy(overrides);
+        await slasher.deployed();
+
+
+        console.log("Deploying DelegationManager mock...");
+
+        const DMFactory = await ethers.getContractFactory("DelegationManager");
+        const dm = await DMFactory.deploy(overrides);
+        await dm.deployed();
+
+        console.log("Deploying StrategyManager mock...");
+
+        const SMFactory = await ethers.getContractFactory("StrategyManager");
+        const sm = await SMFactory.deploy(dm.address, overrides);
+        await sm.deployed();
+
+        /////////////////////////////////////////////
 
         console.log("Deploying empty contract...");
 
@@ -78,9 +103,44 @@ describe("LagrangeCommittee", function () {
 
         console.log("Deploying transparent proxy...");
 
-        const TransparentUpgradeableProxyFactory = await ethers.getContractFactory("TransparentUpgradeableProxy");
-        proxy = await TransparentUpgradeableProxyFactory.deploy(emptyContract.address, proxyAdmin.address, "0x");
-        await proxy.deployed();
+        TransparentUpgradeableProxyFactory = await ethers.getContractFactory("TransparentUpgradeableProxy");
+        lcproxy = await TransparentUpgradeableProxyFactory.deploy(emptyContract.address, proxyAdmin.address, "0x", overrides);
+        await lcproxy.deployed();
+
+        console.log("Deploying transparent proxy...");
+
+        TransparentUpgradeableProxyFactory = await ethers.getContractFactory("TransparentUpgradeableProxy");
+        lsproxy = await TransparentUpgradeableProxyFactory.deploy(emptyContract.address, proxyAdmin.address, "0x", overrides);
+        await lsproxy.deployed();
+
+        console.log("Deploying transparent proxy...");
+
+        TransparentUpgradeableProxyFactory = await ethers.getContractFactory("TransparentUpgradeableProxy");
+        lsmproxy = await TransparentUpgradeableProxyFactory.deploy(emptyContract.address, proxyAdmin.address, "0x", overrides);
+        await lsmproxy.deployed();
+
+        console.log("Deploying Lagrange Committee...");
+
+        const LagrangeCommitteeFactory = await ethers.getContractFactory("LagrangeCommittee");
+        const committee = await LagrangeCommitteeFactory.deploy(lsproxy.address, lsmproxy.address, sm.address);
+        await committee.deployed();
+
+
+        console.log("Deploying Lagrange Service Manager...");
+        
+        const LSMFactory = await ethers.getContractFactory("LagrangeServiceManager");
+        const lsm = await LSMFactory.deploy(slasher.address, lcproxy.address, lsproxy.address, overrides);
+        await lsm.deployed();
+
+
+        console.log("Deploying Lagrange Service...");
+
+        const LSFactory = await ethers.getContractFactory("LagrangeService",{});
+        const lagrangeService = await LSFactory.deploy(lcproxy.address, lsm.address, overrides);
+        await lagrangeService.deployed();
+        lsaddr = lagrangeService.address;
+
+        /////////////////////////////////////////////
 
         console.log("Upgrading proxy...");
 
@@ -89,7 +149,51 @@ describe("LagrangeCommittee", function () {
             committee.address,
             committee.interface.encodeFunctionData("initialize", [admin.address, poseidonAddresses[1], poseidonAddresses[2], poseidonAddresses[3], poseidonAddresses[4], poseidonAddresses[5], poseidonAddresses[6]])
         )
+
+
+        /////////////////////////////////////////////
         
+        //console.log("Loading Lagrange Committee shared state...");
+
+        //lc = shared.LagrangeCommittee;
+        
+        /////////////////////////////////////////////
+        
+        const outboxFactory = await ethers.getContractFactory("Outbox");
+        const outbox = await outboxFactory.deploy();
+        await outbox.deployed();
+        outboxAddr = outbox.address;
+
+        const l2ooFactory = await ethers.getContractFactory("L2OutputOracle");
+        const l2oo = await l2ooFactory.deploy(
+            1,//_submissionInterval
+            1,//_l2BlockTime
+            11991388-1,//_startingBlockNumber
+            1,//_startingTimestamp
+            "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",//_proposer
+            "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",//_challenger
+            5//_finalizationPeriodSeconds
+        );
+        await l2oo.deployed();
+        l2ooAddr = l2oo.address;
+        
+        const ovFactory = await ethers.getContractFactory("OptimismVerifier");
+        const opt = await ovFactory.deploy(l2oo.address);
+        
+        const avFactory = await ethers.getContractFactory("ArbitrumVerifier");
+        const arb = await avFactory.deploy(outbox.address);
+        
+        console.log("L2OutputOracle:",l2oo.address);
+        console.log("Outbox:",outbox.address);
+        
+        await lagrangeService.setOptAddr(opt.address);
+        await lagrangeService.setArbAddr(arb.address);
+
+        console.log("OptimismVerifier:",opt.address);
+        console.log("ArbitrumVerifier:",arb.address);
+        
+        /////////////////////////////////////////////
+
         shared.LagrangeCommittee = committee;
     });
 
