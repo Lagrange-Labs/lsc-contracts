6b7cddfbeb1b616affcf5976b44fa396584bf908
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-09-04 14:45:04 -0500

    enforce timeout, specify signer in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,18 @@ function getRemappings() {
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.12",
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  mocha: {
+    timeout: 100000000,
+  },
   preprocess: {
     eachLine: (hre) => ({
       transform: (line) => {
@@ -39,9 +50,21 @@ module.exports = {
       gas: "auto", // Automatically estimate the gas in each transaction
       blockGasLimit: 0x1fffffffffffff, // Set a high block gas limit
       allowUnlimitedContractSize: true, // This option disables the contract size check
+      accounts: [
+        {
+          privateKey:
+            "0x3e17bc938ec10c865fc4e2d049902716dc0712b5b0e688b7183c16807234a84c",
+          balance: "1000000000000000000000000",
+        },
+      ],
     },
     docker: {
       url: "http://0.0.0.0:8545",
+      allowUnlimitedContractSize: true, // This option disables the contract size check
+      timeout: 100000000,
+      accounts: [
+        "0x3e17bc938ec10c865fc4e2d049902716dc0712b5b0e688b7183c16807234a84c",
+      ],
     },
   },
 };
