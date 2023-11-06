aba64c5aaf30775b48f2c006d757156b4e12b28b
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-11-06 08:51:40 -0600

    add distributor to deployment flow, fix deploy verifiers script

--- util/deploy-verifiers.js
+++ util/deploy-verifiers.js
@@ -12,37 +12,10 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-  // deploy - single signature
-
-  let triageABIraw = fs.readFileSync(
-    'out/SlashingSingleVerifierTriage.sol/SlashingSingleVerifierTriage.json',
-  );
-  let triageABIjson = JSON.parse(triageABIraw);
-  let triageABI = triageABIjson.abi;
-  let triage = new ethers.Contract(addresses['SigVerify'], triageABI, wallet);
-
-  // deploy
-  path = 'src/library/slashing_single/verifier.sol:Verifier';
-  factory = await ethers.getContractFactory(path);
-  verifier = await factory.deploy();
-  tx = await verifier.deployed();
-  console.log(
-    'slashing_single verifier contract deployed to address',
-    verifier.address,
-  );
-
-  // associate
-  tx = await triage.setRoute(1, verifier.address);
-  console.log(
-    'verifier contract associated with triage contract at',
-    triage.address,
-  );
-  await tx.wait();
-
   // deploy - aggregate
 
   triageABIraw = fs.readFileSync(
-    'out/SlashingAggregate16VerifierTriage.sol/SlashingAggregate16VerifierTriage.json',
+    'out/SlashingAggregateVerifierTriage.sol/SlashingAggregateVerifierTriage.json',
   );
   triageABIjson = JSON.parse(triageABIraw);
   triageABI = triageABIjson.abi;
@@ -51,7 +24,7 @@ async function main() {
   const sizes = [16, 32, 64];
   for (size of sizes) {
     // deploy
-    path = `src/library/slashing_aggregate_${size}/verifier.sol:Verifier`;
+    path = `src/library/slashing_aggregate/verifier_${size}.sol:Verifier_${size}`;
     factory = await ethers.getContractFactory(path);
     verifier = await factory.deploy();
     tx = await verifier.deployed();
