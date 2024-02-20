8cfaafc23e26ca1f6ec1ea2606f721133618aec0
cool-developer <51834436+cool-develope@users.noreply.github.com>
2024-02-20 15:16:10 -0500

    fix zero voting power (#61)

--- test/foundry/CommitteeTree.t.sol
+++ test/foundry/CommitteeTree.t.sol
@@ -39,6 +39,37 @@ contract CommitteeTreeTest is LagrangeDeployer {
         vm.stopPrank();
     }
 
+    function testSubscribeChain() public {
+        uint256[2] memory blsPubKey;
+        blsPubKey = [uint256(1), 2];
+        address operator = vm.addr(101);
+
+        vm.deal(operator, 1e19);
+        // add operator to whitelist
+        vm.prank(vm.addr(1));
+        address[] memory operators = new address[](1);
+        operators[0] = operator;
+        lagrangeService.addOperatorsToWhitelist(operators);
+
+        _deposit(operator, 1e5); // which is less than the dividend threshold, the voting power is 0
+
+        vm.startPrank(operator);
+
+        // register operator
+        vm.roll(START_EPOCH + EPOCH_PERIOD - FREEZE_DURATION - 1);
+        lagrangeService.register(blsPubKey);
+        // subscribe chain
+        vm.expectRevert("The operator has no voting power.");
+        lagrangeService.subscribe(CHAIN_ID);
+        vm.stopPrank();
+
+        _deposit(operator, 1e9); // now the voting power is 1
+
+        vm.startPrank(operator);
+        lagrangeService.subscribe(CHAIN_ID);
+        vm.stopPrank();
+    }
+
     function testTreeConstruct() public {
         uint256[2] memory blsPubKey;
         blsPubKey = [uint256(1), 2];
@@ -55,9 +86,9 @@ contract CommitteeTreeTest is LagrangeDeployer {
         (ILagrangeCommittee.CommitteeData memory cur, bytes32 next) =
             lagrangeCommittee.getCommittee(CHAIN_ID, START_EPOCH + EPOCH_PERIOD);
 
-        assertEq(cur.totalVotingPower, 6e12);
+        assertEq(cur.totalVotingPower, 6e6);
         assertEq(cur.leafCount, 3);
-        assertEq(cur.root, 0xb2aab5a90ede2eac50608f62708bfcb3591a86311acfc5d28f927a97a9bc4379);
+        assertEq(cur.root, 0xb36023a1020f51f4b8ba6238d383002481e1dcce915043fecd5d2159513808e3);
         assertEq(cur.root, next);
 
         // update the amount of operator 1
@@ -76,9 +107,9 @@ contract CommitteeTreeTest is LagrangeDeployer {
         lagrangeCommittee.update(CHAIN_ID, 2);
 
         (cur, next) = lagrangeCommittee.getCommittee(CHAIN_ID, START_EPOCH + EPOCH_PERIOD * 2);
-        assertEq(cur.totalVotingPower, 7e12);
+        assertEq(cur.totalVotingPower, 7e6);
         assertEq(cur.leafCount, 3);
-        assertEq(cur.root, 0x058674bb77a6ad5e3df6288b22318f1bbc580273be644666b778125b6b28df89);
+        assertEq(cur.root, 0x02f507202eec14a32171bbca5d048778e4c67238b21037a90b90608c71b6276a);
         assertEq(cur.root, next);
     }
 }
