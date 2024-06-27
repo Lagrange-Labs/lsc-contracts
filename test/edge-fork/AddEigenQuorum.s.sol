b4467105ab77be8f32349c32c9756b9d0b84c078
ChainDev931105 <ethanackerland@gmail.com>
2024-06-27 12:43:59 -0500

    test voteweigher for eigen quorum

--- test/edge-fork/AddEigenQuorum.s.sol
+++ test/edge-fork/AddEigenQuorum.s.sol
@@ -1,49 +1,103 @@
 pragma solidity ^0.8.12;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IStrategyManager} from "eigenlayer-contracts/src/contracts/interfaces/IStrategyManager.sol";
+import {IDelegationManager} from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import {IStrategy} from "eigenlayer-contracts/src/contracts/interfaces/IStrategy.sol";
 import "../../script/update/BaseScript.s.sol";
 import "../../contracts/interfaces/IVoteWeigher.sol";
 
-interface IEigenStrategy {
+interface IEigenStrategy is IStrategy {
     function EIGEN() external view returns (address);
+
+    function strategyManager() external view returns (address);
 }
 
 contract AddEigenQuorum is BaseScript {
     function run() public {
         _readContracts();
 
-        address eigenStrategy = _getEigenStrategyAddress();
+        IEigenStrategy eigenStrategy = _getEigenStrategyAddress();
+
+        // Register Quorum to VoteWeigher
+        uint96 multiplier = 1000000000;
+        uint8 quorumNumber = 1;
 
         IVoteWeigher.TokenMultiplier[] memory multipliers = new IVoteWeigher.TokenMultiplier[](1);
-        multipliers[0].token = eigenStrategy;
-        multipliers[0].multiplier = 1000000000;
+        multipliers[0].token = address(eigenStrategy);
+        multipliers[0].multiplier = multiplier;
 
         vm.prank(voteWeigher.owner());
+        voteWeigher.addQuorumMultiplier(quorumNumber, multipliers);
 
-        voteWeigher.addQuorumMultiplier(1, multipliers);
+        // Check if registered correctly
+        {
+            // calling multiplier index 0 should return above value
+            (address _token, uint256 _multiplier) = voteWeigher.quorumMultipliers(quorumNumber, 0);
+            assertEq(_token, address(eigenStrategy));
+            assertEq(_multiplier, multiplier);
 
-        {
-            (address _token, uint256 _multiplier) = voteWeigher.quorumMultipliers(1, 0);
-            assertEq(_token, eigenStrategy);
-            assertEq(_multiplier, 1000000000);
-        }
-        {
+            // calling multiplier index 1 should revert
             vm.expectRevert();
-            voteWeigher.quorumMultipliers(1, 1);
+            voteWeigher.quorumMultipliers(quorumNumber, 1);
         }
+        
 
-        console.log(_getEigenTokenAddress());
+        // Get bEigenToken & strategyManager
+        IERC20 bEigenToken = _getBEigenToken();
+        IStrategyManager strategyManager = IStrategyManager(eigenStrategy.strategyManager());
+
+        address operator = vm.addr(9876543210); // random address for testing        
+        uint256 amount = 100 * 1e18;
+
+        // airdrop bEigenToken to operator
+        _airdropBEigenToken(operator, amount);
+        
+        vm.startPrank(operator);
+
+        // deposit to strategyManager
+        bEigenToken.approve(address(strategyManager), amount);
+        strategyManager.depositIntoStrategy(IStrategy(address(eigenStrategy)), bEigenToken, amount);
+        
+        // register to delegation
+        IDelegationManager delegation = strategyManager.delegation();
+        IDelegationManager.OperatorDetails memory operatorDetails = IDelegationManager.OperatorDetails({
+            earningsReceiver: operator,
+            delegationApprover: address(0),
+            stakerOptOutWindowBlocks: 0
+        });        
+        delegation.registerAsOperator(operatorDetails, "");
+
+        // check if weightOfOperator returns correct
+        uint96 weight = voteWeigher.weightOfOperator(quorumNumber, operator);
+        uint96 expectedWeight = uint96((uint256(multiplier) * amount) / 1e18);
+        assertEq(weight, expectedWeight, "Incorrect weight returned");
+        
+        vm.stopPrank();
     }
 
-    function _getEigenStrategyAddress() internal view returns (address) {
+    function _getEigenStrategyAddress() internal view returns (IEigenStrategy) {
         if (block.chainid == 1) {
-            return 0xaCB55C530Acdb2849e6d4f36992Cd8c9D50ED8F7;
+            return IEigenStrategy(address(0xaCB55C530Acdb2849e6d4f36992Cd8c9D50ED8F7));
         } else if (block.chainid == 17000) {
-            return 0x43252609bff8a13dFe5e057097f2f45A24387a84;
+            return IEigenStrategy(address(0x43252609bff8a13dFe5e057097f2f45A24387a84));
         }
-        return address(0);
+        return IEigenStrategy(address(0));
     }
 
-    function _getEigenTokenAddress() internal view returns (address) {
-        return IEigenStrategy(_getEigenStrategyAddress()).EIGEN();
+    function _getEigenToken() internal view returns (IERC20) {
+        return IERC20(_getEigenStrategyAddress().EIGEN());
+    }
+
+    function _getBEigenToken() internal view returns (IERC20) {
+        return IERC20(_getEigenStrategyAddress().underlyingToken());
+    }
+
+    function _airdropBEigenToken(address to, uint256 amount) internal {
+        IERC20 bEigenToken = _getBEigenToken();
+        address airdropFrom = address(_getEigenToken());
+
+        vm.prank(airdropFrom);
+        bEigenToken.transfer(to, amount);
     }
 }
